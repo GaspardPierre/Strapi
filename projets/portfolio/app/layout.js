a8f22911d2e8cbcf1451397015ec2a856f7145aa
GaspardPierre <pierredillard@gmail.com>
2024-01-23 16:09:09 +0100

    mete + title

--- projets/portfolio/app/layout.js
+++ projets/portfolio/app/layout.js
@@ -4,8 +4,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Portfolio Pierre',
-  description: 'Pierre Dillard Portfolio',
+  title: 'Portfolio Pierre- Développeur Web-Marly le Roi',
+  description: 'Pierre Dillard est un développeur  Web, basé à Marly-le-roi (78160)',
 }
 
 export default function RootLayout({ children }) {
